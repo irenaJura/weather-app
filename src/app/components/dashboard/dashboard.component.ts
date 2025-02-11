c79e79932ec3ea769180a0611b7ea0d569bf9c03
Irena Jurasek <irenaskydive@gmail.com>
2025-02-11 17:45:11 +0100

    add loading and error handling to dashboard component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { WeatherData } from '../../models/weather-data.model';
 import { WeatherService } from '../../services/weather.service';
 import { CommonModule } from '@angular/common';
-import { forkJoin } from 'rxjs';
+import { catchError, EMPTY, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+  ],
   providers: [WeatherService],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
@@ -29,19 +35,37 @@ export class DashboardComponent {
     { city: 'Tokyo', lat: 35.682839, lon: 139.759455 },
   ];
 
+  isLoading = true;
+
   ngOnInit() {
     this.fetchWeatherData();
   }
 
   fetchWeatherData() {
     const requests = this.testWeatherData.map((cityData) =>
-      this.weatherService.getWeatherData(cityData.lat, cityData.lon)
+      this.weatherService.getWeatherData(cityData.lat, cityData.lon).pipe(
+        catchError((error) => {
+          this.isLoading = false;
+          return EMPTY;
+        })
+      )
     );
 
-    forkJoin(requests).subscribe((results) => {
-      results.forEach((data, index) => {
-        this.testWeatherData[index].data = data;
+    forkJoin(requests)
+      .pipe(
+        catchError((error) => {
+          this.isLoading = false;
+          return EMPTY;
+        })
+      )
+      .subscribe({
+        next: (results) => {
+          results.forEach(
+            (data, index) => (this.testWeatherData[index].data = data)
+          );
+
+          this.isLoading = false;
+        },
       });
-    });
   }
 }
