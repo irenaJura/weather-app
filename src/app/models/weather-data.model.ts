0016c8e5481ad02940c03407a1ca54208e9d9233
Irena Jurasek <irenaskydive@gmail.com>
2025-02-11 10:49:58 +0100

    create weather data model

---
+++ src/app/models/weather-data.model.ts
@@ -0,0 +1,24 @@
+export interface WeatherData {
+  current: {
+    temp: number;
+    feels_like: number;
+    humidity: number;
+    wind_speed: number;
+    weather: {
+      description: string;
+      icon: string;
+    }[];
+  };
+
+  daily: {
+    dt: number;
+    temp: {
+      max: number;
+      min: number;
+    };
+    weather: {
+      description: string;
+      icon: string;
+    }[];
+  }[];
+}
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../env/environment';
+import { WeatherData } from '../models/weather-data.model';
 
 @Injectable({
   providedIn: 'root',
@@ -14,10 +15,10 @@ export class WeatherService {
   getWeatherData(
     lat: number,
     lon: number,
-    exclude: string = '',
-    units: string = 'metric',
-    lang: string = 'en'
-  ): Observable<any> {
+    exclude = 'minutely,hourly',
+    units = 'metric',
+    lang = 'en'
+  ): Observable<WeatherData> {
     const params = new HttpParams()
       .set('lat', lat.toString())
       .set('lon', lon.toString())
@@ -26,6 +27,6 @@ export class WeatherService {
       .set('units', units)
       .set('lang', lang);
 
-    return this.http.get(this.apiUrl, { params });
+    return this.http.get<WeatherData>(this.apiUrl, { params });
   }
 }
