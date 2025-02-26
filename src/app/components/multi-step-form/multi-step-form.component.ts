3e911abfd9c91568c755ff674c731e11a1201035
Irena Jurasek <irenaskydive@gmail.com>
2025-02-26 17:48:49 +0100

    add selected cities chips

--- src/app/components/multi-step-form/multi-step-form.component.ts
+++ src/app/components/multi-step-form/multi-step-form.component.ts
@@ -8,11 +8,16 @@ import {
   debounceTime,
   distinctUntilChanged,
   EMPTY,
+  finalize,
   Observable,
+  of,
   switchMap,
+  tap,
 } from 'rxjs';
 import { WeatherService } from '../../services/weather.service';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'multi-step-form',
@@ -25,32 +30,45 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
     MatFormFieldModule,
     MatInputModule,
     MatAutocompleteModule,
+    MatChipsModule,
+    MatIconModule,
   ],
 })
 export class MultiStepFormComponent {
   cityControl = new FormControl('');
   citySuggestions$: Observable<any[]> = new Observable();
-  selectedCity = '';
+  selectedCities: string[] = [];
+  isLoading = false;
   private weatherService = inject(WeatherService);
 
   ngOnInit(): void {
     this.citySuggestions$ = this.cityControl.valueChanges.pipe(
       debounceTime(300),
       distinctUntilChanged(),
+      tap(() => (this.isLoading = true)),
       switchMap((value) => {
-        if (value && value.length > 2 && value !== this.selectedCity) {
+        if (value && value.length > 2 && !this.selectedCities.includes(value)) {
           return this.weatherService.getCitySuggestions(value).pipe(
             catchError((error) => {
               return EMPTY;
-            })
+            }),
+            finalize(() => (this.isLoading = false))
           );
         }
+        this.isLoading = false;
         return EMPTY;
       })
     );
   }
 
   onCitySelect(city: string): void {
-    this.selectedCity = city;
+    if (!this.selectedCities.includes(city)) {
+      this.selectedCities.push(city);
+      this.cityControl.setValue('');
+    }
+  }
+
+  removeCity(city: string): void {
+    this.selectedCities = this.selectedCities.filter((c) => c !== city);
   }
 }
